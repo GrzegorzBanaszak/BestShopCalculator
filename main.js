ad5921dde3c68deaee729912c7a9cdefd81077df
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-01-20 18:28:34 +0100

    Sekcja wybory i dodanie komentarzy

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
+//Uchwyty
 const totalPrice = document.querySelector("#total-price")
+const packageInput = document.querySelector('#package').children.item(0);
+const packageList = document.querySelector('#package').children.item(1);
+const accounting = document.querySelector('#accounting')
+const terminal = document.querySelector('#terminal');
 
+//Tablica wartości sumy
 let values = [
   {
     name:"products",
-    quantity:0
+    type:"input",
+    price:0
   },
   {
     name:"orders",
-    quantity:0
+    type:"input",
+    price:0
+  },
+  {
+    name:"orders",
+    type:"package",
+    price:0
   },
 ]
 
+
+//Funkcja obsługi inputów
 function setInputFilter(textbox, inputFilter,itemText) {
   ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
     textbox.addEventListener(event, function() {
@@ -40,20 +55,21 @@ function setInputFilter(textbox, inputFilter,itemText) {
   });
 }
 
-
+//Zmiana wartości w tablicy sumy
 function getNewValues(item){
   const newValues = values.filter(x => x.name != item.id)
-  values = [...newValues,{name:`${item.id}`,quantity:item.value * 0.5}]
+  values = [...newValues,{name:`${item.id}`,price:item.value * 0.5}]
 }
 
+//Resetowanie wartości w tablicy sumy
 function resetValue(item){
   const newValues = values.filter(x => x.name != item.id)
-  values = [...newValues,{name:`${item.id}`,quantity:0}]
+  values = [...newValues,{name:`${item.id}`,price:0}]
 }
 
-
+//Zliczenie wartości z tablicy sumy i wyświetlanie 
 function updateTotalPrice(){
-  let total = values.map(x => {return x.quantity}).reduce((a,b) => a + b)
+  let total = values.map(x => {return x.price}).reduce((a,b) => a + b)
   if (total > 0){
     totalPrice.style.display = "block"
     totalPrice.children.item(1).innerText = `$${total}`
@@ -63,7 +79,7 @@ function updateTotalPrice(){
   }
 }
 
-
+//Uchwyty dla inputow
 setInputFilter(document.querySelector('#products'), function(value) {
   return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
 },document.querySelector("[data-id='products']"));
@@ -71,5 +87,52 @@ setInputFilter(document.querySelector('#products'), function(value) {
 setInputFilter(document.querySelector('#orders'), function(value) {
   return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
 },document.querySelector("[data-id='orders']"));
+
+
+//Pokazywanie listy package
+packageInput.addEventListener('click',() =>{
+  if(packageList.style.display == "block"){
+    packageList.style.display = "none"
+  }else{
+    packageList.style.display = "block"
+  }
+})
+
+//lista pakietów
+const packageTypeList = [
+  {
+    name:'basic',
+    price:0
+  },
+  {
+    name:'professional',
+    price:25
+  },
+  {
+    name:'premium',
+    price:60
+  },
+]
+
+//Wybranie pakietu
+Array.from(packageList.children).forEach(item =>{
+  item.addEventListener('click',(e) => {
+    let select = packageTypeList.find(x => x.name == e.target.dataset.value)
+    updatePackage(select)
+    packageInput.innerText = select.name.charAt(0).toUpperCase() + select.name.slice(1);
+    packageList.style.display = "none";
+    updateTotalPrice() 
+  })
+})
+
+//Aktualizacja pakietu w liscie sumy
+function updatePackage({name,price}){
+  const newList = values.filter(x => x.type !== "package")
+  values = [...newList,{
+    name,
+    type:"package",
+    price
+  },]
+}
 
 
